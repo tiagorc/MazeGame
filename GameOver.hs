ad51c52a0ef7675708b768a6c447d86d80cdfed7
Victor Cotrim <cotrim149@gmail.com>
2016-04-16 17:22:33 -0300

    Adding method to calculate time between start and finish game #1

--- GameOver.hs
+++ GameOver.hs
@@ -1,1 +1,17 @@
 module GameOver(setTimer) where
+
+import Data.Time
+import System.IO
+
+configTimerWith time nowTime = addUTCTime (realToFrac (time*60)) nowTime
+
+setTimer = do
+  putStr "Informe tempo de jogo(minutos):"
+  timeGame <- readLn::IO Integer
+  do{putStr "Tempo setado: ";print timeGame;}
+
+  currentTime <- getCurrentTime
+  let time = configTimerWith timeGame currentTime
+
+  do{putStr "Timer setted with success\n"}
+  return time
