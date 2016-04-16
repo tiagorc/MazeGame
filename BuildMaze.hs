9db08be6eddaf74436a23ee949bb5587e73ec4cc
Ruyther Costa <ruy_fusion@hotmail.com>
2016-04-16 11:18:46 -0300

    Add the function to create the matrix

---
+++ BuildMaze.hs
@@ -0,0 +1,5 @@
+module BuildMaze(maze) where
+
+import Data.Matrix
+
+maze = fromList 3 4 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
---
+++ Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import BuildMaze (maze)
+import System.IO
+
+main = do
+
+  print maze
