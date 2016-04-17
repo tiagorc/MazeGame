3ca12e513aaa78a0fde1254c4192e4c6ed0108c2
tiagorc <tiagobrasil2005@yahoo.com.br>
2016-04-17 18:20:45 -0300

    Fix format to add Movement for Int Tuple #3

--- MovementStack.hs
+++ MovementStack.hs
@@ -8,12 +8,12 @@ data MovementsStack position = EmptyStack | MovementsStack [position]
 backToBeginning :: MovementsStack position -> MovementsStack position
 backToBeginning _ = EmptyStack
 
-addNewMovement :: position -> MovementsStack position -> MovementsStack position
+addNewMovement :: (Int,Int) -> MovementsStack (Int,Int) -> MovementsStack (Int,Int)
 addNewMovement value (EmptyStack) = MovementsStack[value]
 addNewMovement value (MovementsStack []) = MovementsStack[value]
 addNewMovement value (MovementsStack complement) = MovementsStack(value:complement)
 
-removeMovement :: MovementsStack position -> MovementsStack position
+removeMovement :: MovementsStack (Int,Int) -> MovementsStack (Int,Int)
 removeMovement (EmptyStack) = error "Voce ja esta no inicio do jogo"
 removeMovement (MovementsStack []) = error "Voce ja esta no inicio do jogo"
 removeMovement (MovementsStack(x:y)) = MovementsStack y
