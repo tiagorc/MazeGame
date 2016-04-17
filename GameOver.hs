efd69eae31dcb40f5b2aacdbef0e12eabfcf8380
Victor Cotrim <cotrim149@gmail.com>
2016-04-17 13:36:22 -0300

    Doing conversion of Date to String, putting Game Over conditions and fix conversion ambiguity #1

--- GameOver.hs
+++ GameOver.hs
@@ -1,11 +1,30 @@
-module GameOver(setTimer) where
+module GameOver(setTimer,isGameOver) where
 
 import Data.Time
 import System.IO
-import Control.Monad
+import qualified Text.Regex as R
 
+-- private?
 configTimerWith time nowTime = addUTCTime (realToFrac (time*60)) nowTime
+saveTime time = writeFile ".time" time
+loadTime = (readFile ".time")
 
+-- back a list with this format ["hh","mm"]
+getTimeFromStringDate stringDate = do
+  let listDate = R.splitRegex (R.mkRegex ":") stringDate
+  let firstCell = listDate !! 0
+  let dateHourList = R.splitRegex (R.mkRegex " ") firstCell
+  let hours = dateHourList !! 1
+  let minutes = listDate !! 1
+  [hours,minutes]
+
+-- gameOverConditions ::(savedHour) (actualHour)
+-- gameOverConditions (h1:m1) (h2:m2) = do
+--  if (read h1 :: Int) == (read h2 :: Int)
+--  then return True
+--  print m1
+
+-- Public ?
 setTimer = do
   putStr "Informe tempo de jogo(minutos):"
   timeGame <- readLn::IO Integer
@@ -17,17 +36,13 @@ setTimer = do
   do{putStr "Timer setted with success\n"}
   saveTime (show time)
 
---  getCurrentTime >>= \currentTime -> configTimerWith timeGame currentTime
-
-saveTime time = writeFile ".time" time
-
-compareTime = do
-  savedTime <- (readFile ".time")
+isGameOver = do
+  savedTime <- loadTime
+  let savedTimeList = getTimeFromStringDate savedTime
   currentTime <- getCurrentTime
-  if (show savedTime) == show currentTime
-  then putStr "Time Over\n"
-  else putStr "Time not Over\n"
-  do {putStr "Saved Time: "; print savedTime;}
-  do {putStr "Now Time: "; print (show currentTime);}
-
-doubleX x = if x > 100 then x else x*2
+  let currentTimeList = getTimeFromStringDate (show currentTime)
+--  if (savedTimeList ) == show currentTime
+--  then putStr "Time Over\n"
+--  else putStr "Time not Over\n"
+  do {putStr "Saved Time: "; print savedTimeList;}
+  do {putStr "Now Time: "; print currentTimeList;}
