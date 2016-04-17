05dbefd73be6c570f32a4b2283c5a5c55c590836
Ruyther Costa <ruy_fusion@hotmail.com>
2016-04-16 23:36:36 -0300

    Stated build the menu

--- Main.hs
+++ Main.hs
@@ -2,7 +2,21 @@ module Main where
 
 import BuildMaze (maze)
 import System.IO
+import System.Exit
+import Control.Monad
 
 main = do
+  putStrLn "\n1 - Play"
+  putStrLn "2 - Quit\n"
+  choice <- getLine
+  if choice == "1"
+  then forever (printMenu >> readChoice >>= menuAction)
+  else exitSuccess
 
-  print maze
+printMenu = putStr "Enter the coordinate: \n(m) - Return"
+
+readChoice = hSetBuffering stdin NoBuffering >> hSetEcho stdin False >> getChar
+
+menuAction 'p' = putStrLn "\nHello, world!"
+menuAction 'm' = main
+menuAction _ = hPutStrLn stderr "\nInvalid choice."
