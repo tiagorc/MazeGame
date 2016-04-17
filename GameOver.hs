38c6213c49c349e9763d59bd6ade088b48b2fcd2
Victor Cotrim <cotrim149@gmail.com>
2016-04-17 11:32:08 -0300

    Retriving saved time with file #1

--- GameOver.hs
+++ GameOver.hs
@@ -2,6 +2,7 @@ module GameOver(setTimer) where
 
 import Data.Time
 import System.IO
+import Control.Monad
 
 configTimerWith time nowTime = addUTCTime (realToFrac (time*60)) nowTime
 
@@ -14,4 +15,19 @@ setTimer = do
   let time = configTimerWith timeGame currentTime
 
   do{putStr "Timer setted with success\n"}
-  return time
+  saveTime (show time)
+
+--  getCurrentTime >>= \currentTime -> configTimerWith timeGame currentTime
+
+saveTime time = writeFile ".time" time
+
+compareTime = do
+  savedTime <- (readFile ".time")
+  currentTime <- getCurrentTime
+  if (show savedTime) == show currentTime
+  then putStr "Time Over\n"
+  else putStr "Time not Over\n"
+  do {putStr "Saved Time: "; print savedTime;}
+  do {putStr "Now Time: "; print (show currentTime);}
+
+doubleX x = if x > 100 then x else x*2
