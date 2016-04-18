a01778eda5f4e33fdca60011b88f6fe8ac6b8924
Ruyther Costa <ruy_fusion@hotmail.com>
2016-04-18 15:45:48 -0300

    Deleting the forever function in main and replace to recursion function

--- Main.hs
+++ Main.hs
@@ -6,13 +6,21 @@ import System.Exit
 import Control.Monad
 import ManageMovement (isValid)
 
+playFlow = do {
+  printMaze;
+  printMenu;
+  readChoice >>= isValid;
+  playFlow
+}
+
 main = do
   putStrLn "\n1 - Play"
   putStrLn "2 - Quit\n"
   choice <- getLine
   if choice == "1"
-  then forever (printMaze >> printMenu >> readChoice >>= isValid)
+  then playFlow
   else exitSuccess
+  main
 
 printMaze = print maze
 
@@ -20,10 +28,10 @@ printMenu = putStr "Enter the coordinate: \n(m) - Return"
 
 readChoice = hSetBuffering stdin NoBuffering >> hSetEcho stdin False >> getChar
 
-{- menuAction 'p' = putStrLn "\nHello, world!"
+menuAction 'p' = putStrLn "\nHello, world!"
 menuAction 'm' = main
 menuAction 'w' = isValid 'w'
 menuAction 's' = isValid 's'
 menuAction 'a' = isValid 'a'
 menuAction 'd' = isValid 'd'
-menuAction _ = isValid 'e' -}
+menuAction _ = isValid 'e'
