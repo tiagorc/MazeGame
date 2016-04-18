a9d9885239aee916fad2868dce6a896e84bdebbc
Victor Cotrim <cotrim149@gmail.com>
2016-04-18 16:22:29 -0300

    Try adding increment for tuples and map

--- Main.hs
+++ Main.hs
@@ -6,28 +6,35 @@ import System.Exit
 import Control.Monad
 import ManageMovement (isValid)
 
+sumTuples :: (Integer,Integer) -> (Integer,Integer) -> (Integer,Integer)
+sumTuples (x1,y1) (x2,y2) = (x1+x2,y1+y2)
 
-playFlow = do
+playFlow actualPositionList = do
   printMaze
   printMenu
   choise <- getChar
   let increment = menuAction choise
+  let position = actualPositionList !! 0
   if increment == (-1,-1)
   then main
-  else playFlow
+  else do {
+--    sumTuples position increment
+    playFlow actualPositionList
+    }
 
 main = do
   putStrLn "\n1 - Play"
   putStrLn "2 - Quit\n"
   choice <- getLine
   if choice == "1"
-  then playFlow
+  -- Put a list here in place of (1,1)
+  then playFlow [(1,1)]
   else exitSuccess
   main
 
 printMaze = print maze
 
-printMenu = putStr "Enter the coordinate: \n(m) - Return"
+printMenu = putStr "Enter the coordinate: \n(m) - Return\n"
 
 readChoice = hSetBuffering stdin NoBuffering >> hSetEcho stdin False >> getChar
 
