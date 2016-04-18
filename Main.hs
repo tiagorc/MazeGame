d26e5e214416813b8bcc54a50ea64b91f12cfdd0
Victor Cotrim <cotrim149@gmail.com>
2016-04-18 15:46:33 -0300

    Change map movement return type #3

--- Main.hs
+++ Main.hs
@@ -6,13 +6,24 @@ import System.Exit
 import Control.Monad
 import ManageMovement (isValid)
 
+--forever (printMaze >> printMenu >> readChoice >>= menuAction)
+
+printAndGetMenuChoise = do
+  printMaze
+--  printMenu
+  choise <- getChar
+  let increment = menuAction choise
+  printAndGetMenuChoise
+
+
 main = do
   putStrLn "\n1 - Play"
   putStrLn "2 - Quit\n"
   choice <- getLine
   if choice == "1"
-  then forever (printMaze >> printMenu >> readChoice >>= isValid)
+  then printAndGetMenuChoise
   else exitSuccess
+  main
 
 printMaze = print maze
 
@@ -20,10 +31,12 @@ printMenu = putStr "Enter the coordinate: \n(m) - Return"
 
 readChoice = hSetBuffering stdin NoBuffering >> hSetEcho stdin False >> getChar
 
-{- menuAction 'p' = putStrLn "\nHello, world!"
-menuAction 'm' = main
-menuAction 'w' = isValid 'w'
-menuAction 's' = isValid 's'
-menuAction 'a' = isValid 'a'
-menuAction 'd' = isValid 'd'
-menuAction _ = isValid 'e' -}
+--menuAction :: Maybe Char -> Maybe (Integer,Integer)
+--menuAction 'm' = main
+menuAction direction = isValid direction
+
+
+--menuAction 'w' = isValid 'w'
+--menuAction 's' = isValid 's'
+--menuAction 'a' = isValid 'a'
+--menuAction 'd' = isValid 'd'
--- MovementStack.hs
+++ MovementStack.hs
@@ -19,12 +19,12 @@ removeMovement (MovementsStack(x:y)) = MovementsStack y
 convertListToStack :: [position] -> MovementsStack position
 convertListToStack list = MovementsStack list
 
---mapMovement :: Char -> String
+mapMovement :: Char -> (Integer,Integer)
 --mapMovement direction (EmptyStack) = MovementStack [d]
 --mapMovement direction (MovementStack []) = MovementStack [d]
 mapMovement direction
-  | direction == 'w' = putStrLn "\nfoi pra cima"
-  | direction == 's' = putStrLn "\nfoi pra baixo"
-  | direction == 'a' = putStrLn "\nfoi para a esquerda"
-  | direction == 'd' = putStrLn "\nfoi para a direita"
-  | otherwise = error "\nMovimento Invalido!"
+  | direction == 'w' = (0,1)
+  | direction == 's' = (0,-1)
+  | direction == 'a' = (-1,0)
+  | direction == 'd' = (1,0)
+--  | otherwise = Left "Movimento invalido"
