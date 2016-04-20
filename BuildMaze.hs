afd86baf9b0990b9e39fb91b84fa4027a74ee108
Victor Cotrim <cotrim149@gmail.com>
2016-04-20 01:26:07 -0300

    Adding .maze to git ignore and make convertion of string map to int map from file #3

--- BuildMaze.hs
+++ BuildMaze.hs
@@ -1,5 +1,29 @@
 module BuildMaze(maze) where
 
 import Data.Matrix
+import Data.Char (digitToInt)
+import Data.List.Split
 
-maze = fromList 3 4 [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+-- 0 => caminho livre
+-- 1 => Obstaculo
+-- 2 => ponto inicial
+-- 3 => ponto final
+maze = fromList 3 4 [1,0,0,0,2,0,0,0,1,0,0,3]
+
+startPoint = (2,1)
+endPoint = (3,4)
+
+cvrtStrListToInt = do
+  str <- (readFile ".maze")
+  let strList = init (splitOneOf "\n " str)
+  let intList = map (read::String->Int) strList
+  return intList
+
+{-maze = do
+  str <- (readFile ".maze")
+  let strList = init (splitOneOf "\n " str)
+  print strList
+  let intList = map (read::String->Int) strList
+  print intList
+--  fromList 3 4 (>>= intList)
+-}
