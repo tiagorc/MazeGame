c471e1dc580f2b947c148e9cfe2b6a7c9fe3712d
Victor Cotrim <cotrim149@gmail.com>
2016-04-23 09:06:36 -0300

    Fix filename to save and load map and reference map

--- BuildMaze.hs
+++ BuildMaze.hs
@@ -5,27 +5,22 @@ import Data.Char (digitToInt)
 import Data.List.Split
 import System.Process
 
+mapFilename = ".map"
+mapReferenceFilename = ".referenceMap"
+
 -- 0 => caminho livre
 -- 1 => Obstaculo
 -- 2 => ponto inicial
 -- 3 => ponto final
 -- 4 => player
+-- TODO: Get number of lines and columns dynamically
 maze intList = fromList 3 4 intList
 
-getMazeMap = do
-  intList <- getIntMap ".map"
-  let mazeMap = maze intList
-  return mazeMap
-
-getReferenceMap = do
-  intList <- getIntMap ".referenceMap"
-  let mazeMap = maze intList
-  return mazeMap
-
+-- TODO: Get start and end point from map
 startPoint = (2,1)
 endPoint = (3,4)
 
------
+--
 
 getIntMap fileName= do
   str <- readFile fileName
@@ -33,7 +28,7 @@ getIntMap fileName= do
   let map = cvrStrListToIntList strList []
   return map
 
----
+{- Setters and Getters -}
 
 setMazeMap matrix = do
   let intList = cvrMatrixToIntList matrix
@@ -41,9 +36,19 @@ setMazeMap matrix = do
   let columns = ncols matrix
   saveMap lines columns intList
 
----
+getMazeMap = do
+  intList <- getIntMap mapFilename
+  let mazeMap = maze intList
+  return mazeMap
 
-saveMap lines columns intList = readProcessWithExitCode "./format_maze" [show lines, show columns, show intList, ".maze"] []
+getReferenceMap = do
+  intList <- getIntMap mapReferenceFilename
+  let mazeMap = maze intList
+  return mazeMap
+
+--
+
+saveMap lines columns intList = readProcessWithExitCode "./format_maze" [show lines, show columns, show intList, mapFilename] []
 
 {- Convert fuctions -}
 
